9aa7bee1d9de0fcd1c6735555c15a55d0d572eb7
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2023-04-07 07:17:16 +0000

    inputlabel

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/input";
 
 
 const Auth = () => {
@@ -6,13 +7,20 @@ const Auth = () => {
         <div className="bg-black w-full h-full lg:bg-opacity-50">
             <nav className="px-12 py-5 ">
                 <img src="/images/logo.png" className="h-12" alt="logo" />
+                </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h2 className="text-white-4xl mb-8 font-semibold">Sign in</h2>
+                        <h2 className="text-white-4xl mb-8 font-semibold">Sign in
+                        </h2>
+                        <div className="flex flex-col gap-4 ">
+                            <Input />
+                        </div>
+                        
                     </div>
                 </div>
-
-            </nav>
+                
+          
+           
         </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 
 
+
 export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+  
     
     </>
   )
