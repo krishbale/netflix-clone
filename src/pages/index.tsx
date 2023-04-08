7014ed5bfcf12da51f31a1ab1473853f43f12a7d
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-04-08 13:49:11 +0545

    routesadding'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
+import  { NextPageContext } from 'next';
+import {signOut,getSession} from 'next-auth/react';
+export async function getServerSideProps(context:NextPageContext){
 
+const session = await getSession(context); 
 
-
+if(!session){
+  return{
+    redirect:{
+      destination:'/auth',
+      permanent:false,
+    }
+  }
+}
+return {
+  props:{}
+}
+}
 export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-  
+  <button className='g-10 w-full bg-white ' onClick={()=> signOut()}>Logout</button>
     
     </>
   )
