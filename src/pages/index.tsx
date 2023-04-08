3926874b359e7a18258a74ae1bcb630fc162f8f2
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-04-08 14:20:39 +0545

    profilecreated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import  { NextPageContext } from 'next';
 import {signOut,getSession} from 'next-auth/react';
 export async function getServerSideProps(context:NextPageContext){
@@ -17,9 +18,11 @@ return {
 }
 }
 export default function Home() {
+  const {data:user} = useCurrentUser();
   return (
     <>
     <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    <p className='text-white'>Logged in As :{user?.name}</p>
   <button className='g-10 w-full bg-white ' onClick={()=> signOut()}>Logout</button>
     
     </>
