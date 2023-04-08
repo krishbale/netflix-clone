b2f6a5fdb0c394cd5becc5c72896eac8d5c8910a
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-04-08 12:44:54 +0545

    loginandregistercompleted

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,6 +1,9 @@
 import Input from "@/components/input";
 import { SetStateAction, useCallback, useState } from "react";
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
+import { getSession, signIn } from 'next-auth/react';
 
 const Auth = () => {
   // const [email,setEmail] = useState('');
@@ -8,9 +11,38 @@ const Auth = () => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [variant, setVariant] = useState('login');
+  const router = useRouter();
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
   }, []);
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+
+      router.push('/profiles');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+      login();
+    } catch (error) {
+        console.log(error);
+    }
+  }, [email, name, password, login]);
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -48,7 +80,7 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover-bg-red-700 transition">
+            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover-bg-red-700 transition">
             {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
