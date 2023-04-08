373857e817e505ad4c701d07c0451fb00fd1ab55
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-04-08 14:39:29 +0545

    navbaritemmobilemenucreated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import  Navbar  from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import  { NextPageContext } from 'next';
-import {signOut,getSession} from 'next-auth/react';
+import {getSession} from 'next-auth/react';
 export async function getServerSideProps(context:NextPageContext){
 
 const session = await getSession(context); 
@@ -18,12 +19,10 @@ return {
 }
 }
 export default function Home() {
-  const {data:user} = useCurrentUser();
+
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-    <p className='text-white'>Logged in As :{user?.name}</p>
-  <button className='g-10 w-full bg-white ' onClick={()=> signOut()}>Logout</button>
+   <Navbar />
     
     </>
   )
